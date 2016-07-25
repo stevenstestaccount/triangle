a23f850eb0e4c30c858c2cc98054ea0cdcf41ae4
meechelle <mfbei@ccanet.rockwellcollins.com>
2016-07-25 08:33:18 -0500

    Initial commit

---
+++ triangleCalculation.py
@@ -0,0 +1,6 @@
+num1 = input("Enter side 1 of triangle: ")
+num2 = input("Enter side 2 of triangle: ")
+num3 = input("Enter side 3 of triangle: ")
+
+sum = num1 + num2 + num3
+print("Total size: ", sum)
